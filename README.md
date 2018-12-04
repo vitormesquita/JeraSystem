1a6c29942d24f6170d333921349e677037533d82
Vitor mesquita <vitor.mesquita09@gmail.com>
2018-12-04 15:24:54 -0300

    initial config

---
+++ README.md
@@ -0,0 +1,1 @@
+# Jera Design System
---
+++ Source/JeraSystem.h
@@ -0,0 +1,19 @@
+//
+//  JeraSystem.h
+//  JeraSystem
+//
+//  Created by Vitor Mesquita on 04/12/18.
+//  Copyright © 2018 Vitor Mesquita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JeraSystem.
+FOUNDATION_EXPORT double JeraSystemVersionNumber;
+
+//! Project version string for JeraSystem.
+FOUNDATION_EXPORT const unsigned char JeraSystemVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JeraSystem/PublicHeader.h>
+
+
