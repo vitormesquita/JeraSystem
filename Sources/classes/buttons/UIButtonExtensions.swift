2fd0b0eee79e15947953a84e3b052704afa203f4
Victor Pereira <victorpereira@jera.com.br>
2018-12-06 11:24:31 -0300

    flat button, default themes

---
+++ Sources/classes/buttons/UIButtonExtensions.swift
@@ -0,0 +1,9 @@
+//
+//  UIButtonExtensions.swift
+//  JeraSystem
+//
+//  Created by Victor Pereira on 06/12/18.
+//  Copyright © 2018 Vitor Mesquita. All rights reserved.
+//
+
+import Foundation
---
+++ Sources/classes/settings/Corner.swift
@@ -0,0 +1,9 @@
+//
+//  Corner.swift
+//  JeraSystem
+//
+//  Created by Victor Pereira on 05/12/18.
+//  Copyright © 2018 Vitor Mesquita. All rights reserved.
+//
+
+import Foundation
