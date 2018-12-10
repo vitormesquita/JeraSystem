10c030707e4603c892d0d24451d051f0878584f3
Victor Pereira <victorpereira@jera.com.br>
2018-12-10 15:26:13 -0300

    DottedButton fix

--- Sources/classes/buttons/DottedButton.swift
+++ Sources/classes/buttons/DottedButton.swift
@@ -6,4 +6,44 @@
 //  Copyright © 2018 Vitor Mesquita. All rights reserved.
 //
 
-import Foundation
+
+import UIKit
+
+@IBDesignable
+class DottedButton: FlatButton {
+    
+    @IBInspectable
+    var cornerRadius: CGFloat = Corner.simple.offset {
+        didSet {
+            layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    @IBInspectable
+    var borderWidth: CGFloat = 1 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable
+    var borderColor: UIColor = ThemeManager.shared.color.primary {
+        didSet {
+            layer.borderColor = borderColor.cgColor
+        }
+    }
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        
+        let path = UIBezierPath(roundedRect: rect, cornerRadius: cornerRadius)
+        path.lineWidth = borderWidth
+        
+        borderColor.setStroke()
+        
+        let dashPattern: [CGFloat] = [4, 4]
+        path.setLineDash(dashPattern, count: 2, phase: 0)
+        path.stroke()
+    }
+    
+}
